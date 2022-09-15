2233c3d409ddd08b1070e00201df85d3c109ed89
Piyush Barve <barvepiyush1000@gmail.com>
2022-09-15 09:45:03 +0000

    new commit

--- the-dojo/src/App.js
+++ the-dojo/src/App.js
@@ -1,9 +1,18 @@
 import './App.css'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+//pages and components
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className='container'>
+            <Switch>
+              
+            </Switch>
+        </div>
+        </BrowserRouter>
     </div>
   );
 }
