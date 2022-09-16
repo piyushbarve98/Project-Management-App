c038181535a9960b8f73e9b1e4430b09579040f8
Piyush Barve <barvepiyush1000@gmail.com>
2022-09-16 13:13:27 +0000

    Project List added

--- the-dojo/src/components/ProjectList.js
+++ the-dojo/src/components/ProjectList.js
@@ -1,14 +1,27 @@
 
 import './ProjectList.css'
-
+import Avatar from './Avatar'
+import {Link} from 'react-router-dom'
 import React from 'react'
 
 export default function ProjectList({projects}) {
   return (
-    <div>
+    <div className='project-list'>
         {projects.length === 0 && <p>No projects yet !</p>}
         {projects.map(project =>(
-            <div key={project.id}>{project.name}</div>
+            <Link key={project.id} to={`/projects/${project.id}`}>
+                <h4>{project.name}</h4>
+                <p>Due by {project.dueDate.toDate().toDateString()}</p>
+                <div className='assigned-to'>
+                  <ul>
+                  {project.assignedUsersList.map(user=>(
+                    <li key={user.photoURL}>
+                      <Avatar src={user.photoURL}/>
+                    </li>
+                  ))}
+                  </ul>
+                </div>
+            </Link>
         ))}
     </div>
   )
---
+++ the-dojo/src/pages/project/ProjectSummary.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Avatar from '../../components/Avatar'
+
+export default function ProjectSummary({project}) {
+  return (
+    <div>
+        <div className='project-summary'>
+            <h2 className='page-title'>{project.name}</h2>
+            <p className='due-date'>
+                Project due by {project.dueDate.toDate().toDateString()}
+            </p>
+            <p className='details'>
+                {project.details}
+            </p>
+            <h4>Projecct is assigned to: </h4>
+            {project.assignedUsersList.map(user =>(
+                <div key={user.id}>
+                    <Avatar  src = {user.photoURL} />
+                </div>
+            ))}
+
+        </div>
+    </div>
+  )
+}
